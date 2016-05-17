80aefa21d65b07f5c67af84bac7e16193158d720
ronstebelton <ronstebelton@yahoo.com>
2016-05-17 18:55:17 -0400

    acs gross rent line chart

---
+++ static/graph-config/acs-gross-rent-percent-household-income3.js
@@ -0,0 +1,16 @@
+var chart = c3.generate({
+    bindto: '#chart6',
+    data: {
+        x: 'Percentage',
+        url: '/static/data/acs-gross-rent-percent-household-income3.csv',
+        type: 'line'
+    },
+    color: {
+        pattern: ['rgba(245, 205, 86, 0.95)',
+                   'rgba(206, 58, 69, 0.93)',
+                   'rgba(4, 90, 139, 0.93)',
+                   'rgba(166, 86, 245, 0.95)',
+                   'rgba(68, 206, 58, 0.93)',
+                   'rgba(139, 49, 4, 0.93)']
+    }
+});
