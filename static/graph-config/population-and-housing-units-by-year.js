fbafb469dc5e2badbf7f3f5c944bcce0a676f21f
ronstebelton <ronstebelton@yahoo.com>
2016-07-02 13:25:18 -0400

    W-16 Change graphs to line

--- static/graph-config/population-and-housing-units-by-year.js
+++ static/graph-config/population-and-housing-units-by-year.js
@@ -3,14 +3,10 @@ var chart = c3.generate({
     data: {
         x: 'Year',
         url: '/static/data/population-and-housing-units-by-year.csv',
-        type: 'bar'
+        type: 'line'
     },
     color: {
-        pattern: ['rgba(245, 205, 86, 0.95)',
-                   'rgba(206, 58, 69, 0.93)',
-                   'rgba(4, 90, 139, 0.93)',
-                   'rgba(166, 86, 245, 0.95)',
-                   'rgba(68, 206, 58, 0.93)',
-                   'rgba(139, 49, 4, 0.93)']
+        pattern: ['#986246',
+                  '#324A63']
     }
 });
