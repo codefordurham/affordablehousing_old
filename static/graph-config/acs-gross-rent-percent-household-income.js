ee506a59ddf22a4b8dc9b3d5df71831f542dd41a
ronstebelton <ronstebelton@yahoo.com>
2016-07-04 13:41:57 -0400

    W-15 Add labels to graphs

--- static/graph-config/acs-gross-rent-percent-household-income.js
+++ static/graph-config/acs-gross-rent-percent-household-income.js
@@ -6,6 +6,24 @@ var chart = c3.generate({
         type: 'line',
         groups: [["Less than 10.0 percent","10.0 to 14.9 percent","15.0 to 19.9 percent","20.0 to 24.9 percent","25.0 to 29.9 percent","30.0 to 34.9 percent","35.0 to 39.9 percent","40.0 to 49.9 percent","50.0 percent or more"]]
     },
+    axis: {
+        x: {
+            label: {
+                text: 'Year',
+                position: 'outer-center'
+            }
+        },
+        y: {
+            label: {
+                text: 'Number of Households',
+                position: 'outer-middle'
+            },
+            min: 0,
+            padding: {
+                bottom: 0
+            }
+        }
+    },
     color: {
         pattern: ['#986246',
                   '#986246',
--- static/graph-config/population-and-housing-units-by-year.js
+++ static/graph-config/population-and-housing-units-by-year.js
@@ -5,6 +5,24 @@ var chart = c3.generate({
         url: '/static/data/population-and-housing-units-by-year.csv',
         type: 'line'
     },
+    axis: {
+        x: {
+            label: {
+                text: 'Year',
+                position: 'outer-center'
+            }
+        },
+        y: {
+            label: {
+                text: 'Count of People / Housing Units',
+                position: 'outer-middle'
+            },
+            min: 0,
+            padding: {
+                bottom: 0
+            }
+        }
+    },
     color: {
         pattern: ['#986246',
                   '#324A63']
